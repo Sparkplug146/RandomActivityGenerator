86e5dacd474c94f1b98ab138ecd8e176dab9531f
Keoni Inniss <ika84465@gmail.com>
2026-03-10 10:46:20 -0400

    Added code within fetch block

--- script.js
+++ script.js
@@ -4,13 +4,21 @@ const setup = document.getElementById("jokeSetup");
 const delivery = document.getElementById("jokeDelivery");
 
 button.addEventListener("click", function(){
-    console.log("Generating Activity...")
 
     fetch("https://v2.jokeapi.dev/joke/Any?safe-mode")
   .then(function (response) {
     return response.json();
   })
   .then(function (data) {
-    console.log(data);
+    
+    category.textContent = "Category: " + data.category;
+    if(data.type === "single"){
+      setup.textContent = "Joke: " + data.joke;
+      delivery.textContent = "";
+    } else {
+      setup.textContent = "Setup: " + data.setup;
+      delivery.textContent = "Delivery: " + statusbar.delivery;
+    }
+    
   });
 })
